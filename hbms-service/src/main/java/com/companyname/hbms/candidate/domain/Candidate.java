91d23a0a774b7995563b7cdc3c767b36d44717d9
fellowlong <WJS493671285wcl>
2014-05-14 18:07:41 +0800

    新建项目

---
+++ hbms-service/src/main/java/com/companyname/hbms/candidate/domain/Candidate.java
@@ -0,0 +1,14 @@
+package com.companyname.hbms.candidate.domain;
+
+import java.io.Serializable;
+
+/**
+ * Created by wangjinsi on 2014-05-14.
+ */
+public class Candidate implements Serializable {
+
+  private Long id;
+
+  private String name;
+
+}
---
+++ hbms-service/src/main/java/com/companyname/hbms/customer/domain/Resume.java
@@ -0,0 +1,11 @@
+package com.companyname.hbms.customer.domain;
+
+import java.io.Serializable;
+
+public class Resume implements Serializable{
+
+  private Long id;
+
+  private String name;
+
+}
