8d2ee24aac22867631c45ab679b79b63b6a97cec
wangjinsi <fellowlong@gmail.com>
2016-12-08 20:06:48 +0800

    添加树的编辑功能

--- hbms-web/src/main/java/com/newstar/hbms/system/web/controller/BaseDataController.java
+++ hbms-web/src/main/java/com/newstar/hbms/system/web/controller/BaseDataController.java
@@ -1,7 +1,11 @@
 package com.newstar.hbms.system.web.controller;
 
+import com.newstar.hbms.basedata.domain.TreeNode;
+import com.newstar.hbms.basedata.service.BaseDataService;
+import com.newstar.hbms.basedata.service.TreeService;
 import com.newstar.hbms.candidate.domain.Candidate;
 import com.newstar.hbms.mvc.ConfigurableMultiActionController;
+import com.newstar.hbms.utils.WebUtils;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,8 +16,22 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class BaseDataController extends ConfigurableMultiActionController {
 
+  private TreeService treeService;
+
+  public void setTreeService(TreeService treeService) {
+    this.treeService = treeService;
+  }
+
   public ModelAndView workspace(HttpServletRequest request, HttpServletResponse response) throws Exception {
     return new ModelAndView("/system/baseDataManager");
+  }
+
+
+  public void findByCode(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    String code = request.getParameter("code");
+    assert code != null && !code.isEmpty();
+    TreeNode treeNode = treeService.findTreeByAncestorCode(code);
+    WebUtils.writeWithJson(response, treeNode);
   }
 
 }
