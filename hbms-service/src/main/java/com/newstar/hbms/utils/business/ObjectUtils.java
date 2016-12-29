9e39bc46e7a624ab10434d18a42d5fb83f458dfa
fellowlong <fellowlong@gmail.com>
2016-12-29 18:35:42 +0800

    编写子对象填充功能

--- hbms-service/src/main/java/com/newstar/hbms/utils/business/ObjectUtils.java
+++ hbms-service/src/main/java/com/newstar/hbms/utils/business/ObjectUtils.java
@@ -1,5 +1,6 @@
 package com.newstar.hbms.utils.business;
 
+import com.newstar.hbms.basedata.domain.TreeNode;
 import com.newstar.hbms.utils.BeanUtils;
 import org.apache.commons.jexl2.Expression;
 import org.apache.commons.jexl2.JexlEngine;
@@ -11,6 +12,229 @@ import java.util.*;
  * Created by fellowlong on 2014/10/1.
  */
 public abstract class ObjectUtils {
+
+  public static class SubObjectConfig {
+
+    private String collectionNameInParent;
+    private String keyNameInParent;
+    private String valueNameInParent;
+    private String keyNameInSubObject;
+    private SubObjectFetcher subObjectFetcher;
+
+    public SubObjectConfig(String keyNameInParent,
+                           String valueNameInParent,
+                           String keyNameInSubObject,
+                           SubObjectFetcher subObjectFetcher) {
+      this(null, keyNameInParent, valueNameInParent, keyNameInSubObject, subObjectFetcher);
+    }
+
+    public SubObjectConfig(String collectionNameInParent,
+                           String keyNameInParent,
+                           String valueNameInParent,
+                           String keyNameInSubObject,
+                           SubObjectFetcher subObjectFetcher) {
+      this.collectionNameInParent = collectionNameInParent;
+      this.keyNameInParent = keyNameInParent;
+      this.valueNameInParent = valueNameInParent;
+      this.keyNameInSubObject = keyNameInSubObject;
+      this.subObjectFetcher = subObjectFetcher;
+    }
+
+    public String getCollectionNameInParent() {
+      return collectionNameInParent;
+    }
+
+    public void setCollectionNameInParent(String collectionNameInParent) {
+      this.collectionNameInParent = collectionNameInParent;
+    }
+
+    public String getKeyNameInParent() {
+      return keyNameInParent;
+    }
+
+    public void setKeyNameInParent(String keyNameInParent) {
+      this.keyNameInParent = keyNameInParent;
+    }
+
+    public String getValueNameInParent() {
+      return valueNameInParent;
+    }
+
+    public void setValueNameInParent(String valueNameInParent) {
+      this.valueNameInParent = valueNameInParent;
+    }
+
+    public String getKeyNameInSubObject() {
+      return keyNameInSubObject;
+    }
+
+    public void setKeyNameInSubObject(String keyNameInSubObject) {
+      this.keyNameInSubObject = keyNameInSubObject;
+    }
+
+    public SubObjectFetcher getSubObjectFetcher() {
+      return subObjectFetcher;
+    }
+
+    public void setSubObjectFetcher(SubObjectFetcher subObjectFetcher) {
+      this.subObjectFetcher = subObjectFetcher;
+    }
+
+    @Override
+    public String toString() {
+      return "SubObjectConfig{" +
+              "collectionNameInParent='" + collectionNameInParent + '\'' +
+              ", keyNameInParent='" + keyNameInParent + '\'' +
+              ", valueNameInParent='" + valueNameInParent + '\'' +
+              ", keyNameInSubObject='" + keyNameInSubObject + '\'' +
+              ", subObjectFetcher=" + subObjectFetcher +
+              '}';
+    }
+  }
+
+
+  public interface SubObjectFetcher {
+
+    public List fetch(List keys);
+
+  }
+
+  public static class ProxySubObjectFetcher implements SubObjectFetcher {
+
+    private SubObjectConfig subObjectConfig;
+
+    private SubObjectFetcher targetSubObjectFetcher;
+
+    public ProxySubObjectFetcher(SubObjectConfig subObjectConfig, SubObjectFetcher targetSubObjectFetcher) {
+      this.subObjectConfig = subObjectConfig;
+      this.targetSubObjectFetcher = targetSubObjectFetcher;
+    }
+
+    @Override
+    public List fetch(List keys) {
+      return targetSubObjectFetcher.fetch(keys);
+    }
+
+    public SubObjectConfig getSubObjectConfig() {
+      return subObjectConfig;
+    }
+
+    public SubObjectFetcher getTargetSubObjectFetcher() {
+      return targetSubObjectFetcher;
+    }
+  }
+
+
+  public static void fillSubObjects(List parents, List<SubObjectConfig> subObjectConfigs) {
+    if (parents == null || parents.isEmpty() || subObjectConfigs == null || subObjectConfigs.isEmpty()) {
+      return;
+    }
+    Map<Object, Map<SubObjectConfig, Object>> parentAndSubObjectMappings =
+        new HashMap<Object, Map<SubObjectConfig, Object>>(parents.size());
+    Map<SubObjectFetcher, Set> keys = new HashMap<SubObjectFetcher, Set>();
+    for (Object parent : parents) {
+      for (SubObjectConfig config : subObjectConfigs) {
+        List parentsOfSubObject = new ArrayList();
+        try {
+          if (config.getCollectionNameInParent() != null && !config.getCollectionNameInParent().isEmpty()) {
+            Collection subCollection = (Collection) BeanUtils.searchValue(parent, config.getCollectionNameInParent());
+            if (subCollection != null && !subCollection.isEmpty()) {
+              parentsOfSubObject.addAll(subCollection);
+            }
+          } else {
+            parentsOfSubObject.add(parent);
+          }
+          for (Object parentOfSubObject : parentsOfSubObject) {
+            Object key = BeanUtils.searchValue(parentOfSubObject, config.getKeyNameInParent());
+            if (key != null) {
+              //存储对象和子对象的映射关系，过滤掉空对象
+              Map<SubObjectConfig, Object> configsOfObject = parentAndSubObjectMappings.get(parentOfSubObject);
+              if (configsOfObject == null) {
+                configsOfObject = new HashMap<SubObjectConfig, Object>(subObjectConfigs.size());
+                parentAndSubObjectMappings.put(parentOfSubObject, configsOfObject);
+              }
+              configsOfObject.put(config, key);
+              //找到子对象的key，用于查找
+              Set keysOfFetcher = keys.get(config.getSubObjectFetcher());
+              if (keysOfFetcher == null) {
+                keysOfFetcher = new HashSet();
+                keys.put(config.getSubObjectFetcher(), keysOfFetcher);
+              }
+              keysOfFetcher.add(key);
+            }
+          }
+        } catch (Exception e) {
+          throw new RuntimeException("获取对象属性失败，o=" + parent.getClass() + ", config=" + config, e);
+        }
+      }
+    }
+    if (keys.isEmpty() || parentAndSubObjectMappings.isEmpty()) {
+      return;
+    }
+
+    //建立fetcher和config的映射关系
+    Map<SubObjectFetcher, List<SubObjectConfig>> fetcherAndConfigMapping =
+        new HashMap<SubObjectFetcher, List<SubObjectConfig>>();
+    for (SubObjectConfig config : subObjectConfigs) {
+      List<SubObjectConfig> configsOfFetcher = fetcherAndConfigMapping.get(config.getSubObjectFetcher());
+      if (configsOfFetcher == null) {
+        configsOfFetcher = new ArrayList<SubObjectConfig>();
+        fetcherAndConfigMapping.put(config.getSubObjectFetcher(), configsOfFetcher);
+      }
+      configsOfFetcher.add(config);
+    }
+
+    //用fetcher抓取子对象
+    Map<SubObjectFetcher, Map> subObjects = new HashMap<SubObjectFetcher, Map>();
+    for (Map.Entry<SubObjectFetcher, Set> entry : keys.entrySet()) {
+      List subObjectsOfFetcher = entry.getKey().fetch(new ArrayList(entry.getValue()));
+      if (subObjectsOfFetcher != null && !subObjectsOfFetcher.isEmpty()) {
+        String keyNameInSubObject = fetcherAndConfigMapping.get(entry.getKey()).get(0).getKeyNameInSubObject();
+        subObjects.put(entry.getKey(), listToMap(subObjectsOfFetcher, keyNameInSubObject));
+      }
+    }
+
+    if (subObjects.isEmpty()) {
+      return;
+    }
+
+    //填充到parent
+    for (Map.Entry<Object, Map<SubObjectConfig, Object>> objectEntry : parentAndSubObjectMappings.entrySet()) {
+      Map<String, Object> subObjectPropertyAndValueMapping = new HashMap<String, Object>();
+      for (Map.Entry<SubObjectConfig, Object> configEntry : objectEntry.getValue().entrySet()) {
+        Map subObjectsOfParent = subObjects.get(configEntry.getKey().getSubObjectFetcher());
+        if (subObjectsOfParent != null) {
+          Object subObject = subObjectsOfParent.get(configEntry.getValue());
+          if (subObject != null) {
+            subObjectPropertyAndValueMapping.put(configEntry.getKey().getValueNameInParent(), subObject);
+          }
+        }
+      }
+      //将找到的属性值绑定到parent上
+      BeanUtils.bindProperties(objectEntry.getKey(), subObjectPropertyAndValueMapping);
+    }
+
+  }//end
+
+  public static Map listToMap(List list, String keyName) {
+    if (list == null || list.isEmpty()) {
+      return null;
+    }
+    Map map = new HashMap(list.size());
+    for (Object o : list) {
+      try {
+        Object key = BeanUtils.searchValue(o, keyName);
+        if (key == null) {
+          throw new Exception("key值为null");
+        }
+        map.put(key, o);
+      } catch (Exception e) {
+        throw new RuntimeException("获取属性值出错:keyName=" + keyName, e);
+      }
+    }
+    return map;
+  }
+
 
   public static void fillAssociationByKey(List parents,
                                           String keyNameInParent,
